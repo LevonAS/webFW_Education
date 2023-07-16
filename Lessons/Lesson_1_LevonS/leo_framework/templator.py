a02c4c33e923f74185522812c436bcc4495e185d
LevonAS <LevonAS@example.com>
2023-07-16 13:16:46 +0300

    lesson_1 done

---
+++ Lessons/Lesson_1_LevonS/leo_framework/templator.py
@@ -0,0 +1,31 @@
+# from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+from os.path import join
+import os, sys
+
+
+project_directory = os.getcwd()
+# sys.path.append(project_directory)
+print("project_directory: ", project_directory)
+print("sys.path: ", sys.path)
+
+
+def render(template_name, folder='templates', **kwargs):
+    """
+    :param template_name: имя шаблона
+    :param folder: папка в которой ищем шаблон
+    :param kwargs: параметры
+    :return:
+    """
+    # file_path = join(folder, template_name)
+    # print("file_path: ",file_path)
+    # # Открываем шаблон по имени
+    # with open(file_path, encoding='utf-8') as f:
+    #     # Читаем
+    #     template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
+    # рендерим шаблон с параметрами
+    return template.render(**kwargs)
