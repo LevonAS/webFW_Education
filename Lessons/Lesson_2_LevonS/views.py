e20f22d56edb2b6acaf96d4abc4eb961b6567c50
LevonAS <LevonAS@example.com>
2023-07-18 01:10:57 +0300

    lesson_2 done

---
+++ Lessons/Lesson_2_LevonS/views.py
@@ -0,0 +1,23 @@
+from leo_framework.templator import render
+
+
+class Index:
+    def __call__(self, request):
+        return '200 OK', render('index.html', date=request.get('date', None))
+
+
+class Products:
+    def __call__(self, request):
+        return '200 OK', render('products.html', date=request.get('date', None))
+
+
+class About:
+    def __call__(self, request):
+        return '200 OK', render('about.html', date=request.get('date', None))
+
+
+class Contact:
+    def __call__(self, request):
+        return '200 OK', render('contact.html', date=request.get('date', None))
+
+
