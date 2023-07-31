66dfac049dd0d1136a7d6886e3ec8e0bd356bbcd
LevonAS <LevonAS@example.com>
2023-07-31 16:59:31 +0300

    lesson_4 done

---
+++ Lessons/Lesson_4_LevonS/leo_framework/main.py
@@ -0,0 +1,83 @@
+from quopri import decodestring
+import re
+
+from .proc_requests import GetRequests, PostRequests
+
+
+class PageNotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+class Framework:
+    """Класс Framework - основа фреймворка"""
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.routes_lst = routes_obj
+        self.fronts_lst = fronts_obj
+
+    def __call__(self, environ, start_response):
+        # получаем адрес, по которому выполнен переход
+        path = environ['PATH_INFO']
+
+        # добавление закрывающего слеша
+        if not path.endswith('/'):
+            path = f'{path}/'
+        print("///сl.Framework/ path: ", path)
+        
+        request = {}
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            data = Framework.decode_value(data)
+            pattern = r"(^[a-zA-Z0-9_.'-]+@[a-zA-Z0-9.-]+\.[a-zA-Z.]{2,4}$)"
+            number_re = re.compile(pattern)
+            if path == "/contact/" and not re.findall(number_re, data['your_email']):
+                print("Вы ввели некорректный email-адрес")
+            else:
+               request['data'] = data 
+               print(f'Нам пришёл post-запрос: {data}')
+
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+
+        # находим нужный контроллер
+        # отработка паттерна page controller
+        if path in self.routes_lst:
+            view = self.routes_lst[path]
+        else:
+            view = PageNotFound404()
+
+        # наполняем словарь request элементами
+        # этот словарь получат все контроллеры
+        # отработка паттерна front controller
+        for front in self.fronts_lst:
+            front(request)
+        # запуск контроллера с передачей объекта request
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        """ Метод меняет не транслируемые символы в запросе на правильные """
+        new_data = {}
+        for k, v in data.items():
+            # print("///m.decode_value/ v: ", v)
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            # print("///m.decode_value/ v_decode_str: ", val_decode_str)
+            new_data[k] = val_decode_str
+        return new_data
+    
+
+# Пример GET-запроса с параметрами
+# http://192.168.42.140:8000/contact/?id=1&category=10
+# Пример полученного GET-запроса с параметрами
+# {'method': 'GET', 'request_params': {'id': '1', 'category': '10'}}
